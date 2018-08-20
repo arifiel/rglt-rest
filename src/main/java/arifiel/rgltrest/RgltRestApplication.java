9b0feee95f20bf9694a55cd2628e71c6758e357b
Vadim Rozanelsky <arifiel@gmail.com>
2018-08-21 01:55:15 +0300

    First commit

---
+++ src/main/java/arifiel/rgltrest/RgltRestApplication.java
@@ -0,0 +1,12 @@
+package arifiel.rgltrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgltRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgltRestApplication.class, args);
+	}
+}
